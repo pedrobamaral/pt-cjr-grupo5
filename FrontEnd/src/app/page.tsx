e5cbb8307aaa1db9d791a3c4790d2bd2c2e2d740
Arthur Fernandes <arthurfsjesus@gmail.com>
2025-06-03 23:27:44 -0300

    update: update in FrontEnd login

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -1,57 +1,74 @@
+"use client";
+import { useFormik } from "formik";
+
 export default function Home() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log("Form values:", values);
+      alert(`Email: ${values.email}\nSenha: ${values.password}`);
+    },
+  });
+
   return (
-    <main className="overflow-hidden">
-      <div className="h-screen flex">
-        {/* Lado da imagem */}
-        <div className="w-[58%] h-full">
-          <img
-            src="images/foto-teste.jpg"
-            alt="Foto-teste"
-            className="object-cover w-full h-full"
+    <main className="h-screen flex">
+      {/* Lado da imagem */}
+      <div className="w-2/5 h-full">
+        <img
+          src="/images/logo.png"
+          alt="Imagem"
+          className="object-cover w-full h-full"
+        />
+      </div>
+
+      {/* Lado do formulário */}
+      <div className="w-3/5 h-full flex justify-center items-center bg-[#15589A]">
+        <div className="w-[350px]">
+         { /*<img
+            src="/images/logo.png"
+            alt="Logo"
+            className="mx-auto mb-6 max-w-[96px] h-auto object-contain"
           />
-        </div>
+         */}
+          <form className="space-y-4" onSubmit={formik.handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-emerald-400 mb-4"
+            />
 
-        {/* Lado do formulário */}
-        <div className="w-[42%] h-full flex justify-center items-center bg-neutral-100">
-          <div className="p-10 rounded-lg w-96 max-w-full">
-            <h1 className="text-3xl font-semibold text-center mb-10">
-              Edu <br /> Ranking
-            </h1>
-    <form className="form-container space-y-6">
-  <div>
-    <input
-      type="email"
-      className="w-full px-4 py-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-emerald-400 placeholder-gray-400"
-      placeholder="Email"
-    />
-  </div>
+            <input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-emerald-400"
+            />
 
-  <div>
-    <input
-      type="password"
-      className="w-full px-4 py-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-emerald-400 placeholder-gray-400"
-      placeholder="Senha"
-    />
-  </div>
+            <div className="flex gap-4 pt-4">
+              <button
+                type="submit"
+                className="flex-1 bg-[#9DCFD8] text-black py-2 rounded-xl border border-[#073f46] shadow-md hover:bg-[#7cf0d3] transition-all"
+              >
+                Entrar
+              </button>
 
-  <div className="flex gap-4">
-    <button
-      type="button"
-      className="flex-1 bg-emerald-200 text-black py-2 rounded-xl border border-blue-900 shadow-md hover:bg-emerald-300 transition-all"
-    >
-      Entrar
-    </button>
-
-    <button
-      type="button"
-      className="flex-1 bg-emerald-200 text-black py-2 rounded-xl border border-blue-900 shadow-md hover:bg-emerald-300 transition-all"
-    >
-      Criar Conta
-    </button>
-  </div>
-</form>
-         
-          </div>
+              <button
+                type="button"
+                className="flex-1 bg-[#9DCFD8] text-black py-2 rounded-xl border border-[#073f46] shadow-md hover:bg-[#7cf0d3] transition-all"
+                onClick={() => alert("Criar conta")}
+              >
+                Criar Conta
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </main>
