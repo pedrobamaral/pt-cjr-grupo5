69abbc32d4eb94a3d57db6f0e0c45f4dfed87518
Pedro <pedrobcamaral@gmail.com>
2025-06-17 16:11:49 -0300

    feat: create delete method

--- backend/src/avaliacao/avaliacao.service.ts
+++ backend/src/avaliacao/avaliacao.service.ts
@@ -18,5 +18,45 @@ export class AvaliacaoService {
     async findAll() {
         return await this.prisma.avaliacao.findMany();
     }
+
+    async update(id: number, data: AvaliacaoDto){
+            const avaliacaoExists = await this.prisma.avaliacao.findUnique({
+                where: {
+                    id,
+                }
+            });
+    
+            if(!avaliacaoExists){
+                throw new Error("Avaliação não encontrada!")
+            }
+    
+            return await this.prisma.avaliacao.update({
+                data,
+                where: {
+                    id, 
+                }
+            });
+        }
+
+        async delete(id: number){
+        const avaliacaoExists = await this.prisma.avaliacao.findUnique({
+            where: {
+                id,
+            }
+        });
+
+        if(!avaliacaoExists){
+            throw new Error("Avaliação não encontrada!")
+        }
+
+        return await this.prisma.avaliacao.delete({
+            where: {
+                id,
+            }
+        });
+    }
+
+
+
     
 }
