5b8b93280711062a247c86dd7d3a35d9d9777698
artmendess <art.mendesborges@gmail.com>
2025-07-10 14:32:18 -0300

    fix: apagando enum emoção import

--- backend/src/avaliacao/dto/avaliacao.dto.ts
+++ backend/src/avaliacao/dto/avaliacao.dto.ts
@@ -1,9 +1,7 @@
-import { Emocao } from "@prisma/client";
 
 export type AvaliacaoDto = {
     usuarioID: number;
     professorID: number;
     disciplinaID: number;
     conteudo: string;
-    humor: Emocao;
 }
